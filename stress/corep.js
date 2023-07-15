2ac1e271a907e0499a40858e2cf6a2319540b51b
Ezequiel Regaldo <eze.swarow@gmail.com>
2023-07-15 02:21:29 -0300

    THE WINNER IS NEST

---
+++ stress/corep.js
@@ -0,0 +1,19 @@
+
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '1s', target: 1000 },
+  ],
+};
+
+export default function () {
+  const res = http.post('http://192.168.1.38:5000/user', JSON.stringify({
+    "firstName": "eze",
+    "lastName": "quiel",
+    "email": "asdasda@asdada.com"
+  }), { headers: { 'Content-Type': 'application/json' } });
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
+}
