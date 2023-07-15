5afe3de29fda800a0c313769a9cf7ff18a65e425
Ezequiel Regaldo <eze.swarow@gmail.com>
2023-07-15 01:21:36 -0300

    change

--- netcoretest/Program.cs
+++ netcoretest/Program.cs
@@ -3,8 +3,6 @@ using netcoretest.Databases;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseQuic();
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
