c0dbf52d53bb92d560fde2027275c56dc858e77c
Ezequiel Regaldo <eze.swarow@gmail.com>
2023-07-14 23:33:16 -0300

    ta a ve

--- nest/src/user/user.entity.ts
+++ nest/src/user/user.entity.ts
@@ -3,9 +3,7 @@ import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn({
-    type: 'bigint'
-  })
+  @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
