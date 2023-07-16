43531ca6d39aa9e80fb0d7f67f750bccf19d124e
Ezequiel Regaldo <eze.swarow@gmail.com>
2023-07-16 01:26:25 -0300

    tata

--- netcoretest/Databases/Postgresql.cs
+++ netcoretest/Databases/Postgresql.cs
@@ -1,19 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using netcoretest.Models;
 
 namespace netcoretest.Databases
 {
     public class Postgresql : DbContext
     {
+        private readonly IMemoryCache _cache;
         public DbSet<User> Users { get; set; } = null!;
-        public Postgresql(DbContextOptions<Postgresql> optionsBuilder) : base(optionsBuilder)
+
+        public Postgresql(DbContextOptions<Postgresql> optionsBuilder, IMemoryCache cache) : base(optionsBuilder)
         {
+            _cache = cache;
             Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMemoryCache(_cache);
+            base.OnConfiguring(optionsBuilder);
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<User>().HasKey(k => k.Id);
+            modelBuilder.Entity<User>().HasIndex(u => u.Id).IsUnique(true);
         }
     }
 }
--- netcoretest/Program.cs
+++ netcoretest/Program.cs
@@ -3,6 +3,8 @@ using netcoretest.Databases;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddMemoryCache();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
