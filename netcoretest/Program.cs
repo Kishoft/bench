73e7b41bfbcc8e331453e9e36f8bc5df59a3cec0
Ezequiel Regaldo <eze.swarow@gmail.com>
2023-07-16 04:14:26 -0300

    gas gas gas

--- netcoretest/Program.cs
+++ netcoretest/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContextPool<Postgresql>(options =>
 {
-    options.UseNpgsql("Host=192.168.1.38;Database=testinpapu;Username=ezequiel;Password=chichito");
+    options.UseNpgsql("Host=postgresql;Database=testinpapu;Username=ezequiel;Password=chichito;Maximum Pool Size=1024;Max Auto Prepare=20");
 });
 
 var app = builder.Build();
