91540452b31c2c58e17d603f316d85ccb7fca3e5
Ezequiel Regaldo <eze.swarow@gmail.com>
2023-07-18 00:43:22 -0300

    ta

--- netcoretest/Program.cs
+++ netcoretest/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<Postgresql>(options =>
 {
-    options.UseNpgsql("Host=postgresql;Database=testinpapu;Username=ezequiel;Password=chichito;Enlist=false;Pooling=true;MinPoolSize=10;MaxPoolSize=800");
+    options.UseNpgsql("Host=postgresql;Database=testinpapu;Username=ezequiel;Password=chichito;Enlist=false;Pooling=true;MinPoolSize=800;MaxPoolSize=12000");
 });
 
 var app = builder.Build();
--- stress/corep.js
+++ stress/corep.js
@@ -4,7 +4,7 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '1s', target: 1000 },
+    { duration: '1s', target: 5000 },
   ],
 };
 
