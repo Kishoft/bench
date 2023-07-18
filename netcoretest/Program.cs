c9563029a78e56fce18833c2e4b3e8ab8479356b
Ezequiel Regaldo <eze.swarow@gmail.com>
2023-07-18 01:09:35 -0300

    Update Program.cs

--- netcoretest/Program.cs
+++ netcoretest/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<Postgresql>(options =>
 {
-    options.UseNpgsql("Host=postgresql;Database=testinpapu;Username=ezequiel;Password=chichito;Pooling=true;MinPoolSize=800;MaxPoolSize=12000;NoResetOnClose=true;MaxAutoPrepare=10;Timeout=1000;CommandTimeout=1000;Keepalive=1;KeepaliveIdle=1;KeepaliveInterval=1");
+    options.UseNpgsql("Host=postgresql;Database=testinpapu;Username=ezequiel;Password=chichito;Pooling=true;MinPoolSize=800;MaxPoolSize=12000;NoResetOnClose=true");
 
 });
 
