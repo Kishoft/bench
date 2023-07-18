bc6b9ff44d6415cdd81df2bb17f3286b2be7bb83
Ezequiel Regaldo <eze.swarow@gmail.com>
2023-07-18 03:08:46 -0300

    gg

--- netcoretest/Program.cs
+++ netcoretest/Program.cs
@@ -3,13 +3,18 @@ using netcoretest.Databases;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options => {
+    options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(10);
+    options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(10);
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContextPool<Postgresql>(options =>
 {
-    options.UseNpgsql("Host=postgresql;Database=testinpapu;Username=ezequiel;Password=chichito;Pooling=true;Enlist=false;MaxPoolSize=1024;");
+    options.UseNpgsql("Host=postgresql;Database=testinpapu;Username=ezequiel;Password=chichito;Pooling=true;Enlist=false;MaxPoolSize=1024;CommandTimeout=0;CancellationTimeout=0");
 });
 
 var app = builder.Build();
