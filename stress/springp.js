b303970964ae1072fe7399d85381577eb598335e
Ezequiel Regaldo <35587335+Kishoft@users.noreply.github.com>
2024-02-02 01:13:30 -0300

    Create springp.js

---
+++ stress/springp.js
@@ -0,0 +1,19 @@
+
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '1s', target: 200 },
+    { duration: '9s', target: 700 },
+  ]
+};
+
+export default function () {
+  const res = http.post('http://127.0.0.1:3300/tttt', JSON.stringify({
+    "name": "eze",
+    "email": "asdasda@asdada.com"
+  }), { headers: { 'Content-Type': 'application/json' } });
+  check(res, { 'status was 204': (r) => r.status == 204 });
+  sleep(1);
+}
