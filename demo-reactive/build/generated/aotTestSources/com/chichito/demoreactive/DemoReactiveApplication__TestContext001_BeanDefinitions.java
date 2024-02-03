e5c1234fce40246275173ad98f19e48d36f0cdbf
Ezequiel Regaldo <35587335+Kishoft@users.noreply.github.com>
2024-02-03 13:10:58 -0300

    gg

---
+++ demo-reactive/build/generated/aotTestSources/com/chichito/demoreactive/DemoReactiveApplication__TestContext001_BeanDefinitions.java
@@ -0,0 +1,23 @@
+package com.chichito.demoreactive;
+
+import org.springframework.aot.generate.Generated;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.annotation.ConfigurationClassUtils;
+
+/**
+ * Bean definitions for {@link DemoReactiveApplication}.
+ */
+@Generated
+public class DemoReactiveApplication__TestContext001_BeanDefinitions {
+  /**
+   * Get the bean definition for 'demoReactiveApplication'.
+   */
+  public static BeanDefinition getDemoReactiveApplicationBeanDefinition() {
+    RootBeanDefinition beanDefinition = new RootBeanDefinition(DemoReactiveApplication.class);
+    beanDefinition.setTargetType(DemoReactiveApplication.class);
+    ConfigurationClassUtils.initializeConfigurationClass(DemoReactiveApplication.class);
+    beanDefinition.setInstanceSupplier(DemoReactiveApplication$$SpringCGLIB$$0::new);
+    return beanDefinition;
+  }
+}
