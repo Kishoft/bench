0b85d53bb1770ca50823527315eb73778d64e289
Ezequiel Regaldo <35587335+Kishoft@users.noreply.github.com>
2024-02-03 13:27:12 -0300

    ta

--- demo-reactive/src/main/java/com/chichito/demoreactive/services/PepeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chichito.demoreactive.services;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.chichito.demoreactive.models.Pepe;
-
-@Repository
-public interface PepeRepository extends ReactiveCrudRepository<Pepe, Long> {}
